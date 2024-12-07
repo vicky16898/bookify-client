750f4af5aa53dfc47aaad2a97f12856fae0178ca
Vicky Daniel <vicky16898@gmail.com>
2024-12-07 13:18:33 -0500

    dep changes

--- src/pages/HostAccommodations.tsx
+++ src/pages/HostAccommodations.tsx
@@ -69,23 +69,24 @@ export default function HostAccommodations() {
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {places.length > 0 &&
         places.map((place) => (
-          <Link to={"/place/" + place._id} key={place._id}>
-            <div className="bg-gray-500 mb-2 rounded-2xl flex">
-              {place.photos?.[0] && (
-                <img
-                  className="rounded-2xl object-cover aspect-square"
-                  src={`${baseAPIPath}/uploads/` + place.photos?.[0]}
-                  alt=""
-                />
-              )}
-            </div>
-            <h2 className="font-bold">{place.address}</h2>
-            <h3 className="text-sm text-black-500">{place.title}</h3>
-            <div className="mt-1">
-              <span className="font-bold">${place.price}</span> per night
-            </div>
-          </Link>
-        ))}
+            <Link to={"/place/" + place._id} key={place._id}>
+              <div className="bg-gray-500 mb-2 rounded-2xl flex">
+                {place.photos?.[0] && (
+                  <img
+                    className="rounded-2xl object-cover aspect-square w-full"
+                    src={`${baseAPIPath}/uploads/` + place.photos?.[0]}
+                    alt=""
+                  />
+                )}
+              </div>
+              <h2 className="font-bold">{place.address}</h2>
+              <h3 className="text-sm text-black-500">{place.title}</h3>
+              <div className="mt-1">
+                <span className="font-bold">${place.price}</span> per night
+              </div>
+            </Link>
+          )
+        )}
     </div>
   );
 }
